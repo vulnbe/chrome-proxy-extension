73452aad350485d14bd3c43656df15cd4fc269f6
Alexey Pronin <a@vuln.be>
2019-09-19 12:45:02 +0300

    Add proxy proto switch

--- popup.js
+++ popup.js
@@ -1,15 +1,14 @@
-var storageProxyEnabled = 'ProxyEnabled';
-var storageProxyIP = 'ProxyIP';
-var storageProxyPort = 'ProxyPort';
 var proxyEnabled = false;
-var checkbox;
-var proxyip;
-var proxyport;
+var proxyEnabledCheckbox;
+var proxyIP;
+var proxyPort;
+var proxyScheme;
 
 var proxyConfig = {
   mode: "fixed_servers",
   rules: {
     singleProxy: {
+      scheme: 'http',
       host: null,
       port: null
     },
@@ -34,73 +33,69 @@ function changeProxyState() {
 }
 function getProxyState(callback) {
   if(chrome.extension.inIncognitoContext){
-    chrome.storage.sync.get([storageProxyEnabled, storageProxyIP, storageProxyPort], (items) => {
-      callback(chrome.runtime.lastError ? null : items[storageProxyEnabled], items[storageProxyIP], items[storageProxyPort]);
+    chrome.storage.sync.get(['proxyEnabled', 'proxyIP', 'proxyPort', 'proxyScheme'], (items) => {
+      callback(chrome.runtime.lastError ? null : items['proxyEnabled'], items['proxyIP'], items['proxyPort'], items['proxyScheme']);
     });
   }
 }
 function saveProxyState() {
   if(chrome.extension.inIncognitoContext){
     var items = {};
-    items[storageProxyEnabled] = proxyEnabled;
-    items[storageProxyIP] = proxyConfig.rules.singleProxy.host;
-    items[storageProxyPort] = proxyConfig.rules.singleProxy.port;
+    items['proxyEnabled'] = proxyEnabled;
+    items['proxyScheme'] = proxyConfig.rules.singleProxy.scheme;
+    items['proxyIP'] = proxyConfig.rules.singleProxy.host;
+    items['proxyPort'] = proxyConfig.rules.singleProxy.port;
     chrome.storage.sync.set(items);
   }
 }
 function processFormEvent() {
   if(chrome.extension.inIncognitoContext){
-    proxyEnabled = checkbox.checked;
-    proxyConfig.rules.singleProxy.host = proxyip.value;
-    proxyConfig.rules.singleProxy.port = parseInt(proxyport.value);
+    proxyEnabled = proxyEnabledCheckbox.checked;
+    proxyConfig.rules.singleProxy.scheme = proxyScheme.value;
+    proxyConfig.rules.singleProxy.host = proxyIP.value;
+    proxyConfig.rules.singleProxy.port = parseInt(proxyPort.value);
     changeProxyState();
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  checkbox = document.getElementById('useproxy');
-  proxyip = document.getElementById('proxyip');
-  proxyport = document.getElementById('proxyport');
+  proxyScheme = document.getElementById('proxyScheme');
+  proxyEnabledCheckbox = document.getElementById('proxyEnabled');
+  proxyIP = document.getElementById('proxyIP');
+  proxyPort = document.getElementById('proxyPort');
   
-  getProxyState((savedState, ip, port) => {
+  getProxyState((savedState, ip, port, scheme) => {
     if (ip) {
-      proxyip.value = ip;
+      proxyIP.value = ip;
       proxyConfig.rules.singleProxy.host = ip;
     }
     if (port) {
-      proxyport.value = port;
+      proxyPort.value = port;
       proxyConfig.rules.singleProxy.port = port;
     }
     if (savedState) {
       proxyEnabled = savedState;
-      checkbox.checked = savedState;
+      proxyEnabledCheckbox.checked = savedState;
+    }
+    if (scheme) {
+      proxyScheme.value = scheme;
+      proxyConfig.rules.singleProxy.scheme = scheme;
     }
     changeProxyState();
   });
   window.addEventListener('focus', () => {
     if(!chrome.extension.inIncognitoContext){
-      document.getElementById('message').setAttribute('class','');
-      document.getElementById('container').setAttribute('class','hidden');
+      document.getElementById('message').setAttribute('class', '');
+      document.getElementById('container').setAttribute('class', 'hidden');
     }
     else{
-      document.getElementById('container').setAttribute('class','container');
-      document.getElementById('message').setAttribute('class','hidden');
+      document.getElementById('container').setAttribute('class', 'container');
+      document.getElementById('message').setAttribute('class', 'hidden');
     }
   });
-  var inputs=document.getElementsByTagName('input'),i=0;
-    do{
-        switch(inputs[i].type){
-            case 'checkbox':
-              inputs[i].onchange=function(){
-                processFormEvent();
-              }
-              break;
-            case 'text':
-              inputs[i].onkeyup=function(){
-                processFormEvent();
-              }
-              break;
-        }
-    }
-    while(inputs[++i])
+
+  proxyScheme.onchange = processFormEvent;
+  proxyEnabledCheckbox.onchange = processFormEvent;
+  proxyIP.onkeyup = processFormEvent;
+  proxyPort.onkeyup = processFormEvent;
 });
